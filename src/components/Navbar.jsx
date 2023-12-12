90aa7bb4635d6f665427bfde9f199e8ba5a9906b
GeniuX <geniuh7@gmail.com>
2023-12-12 13:47:55 -0800

    Commit 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ export default function Navbar() {
 
 
   return (
-    <nav className='sm:right-0 p-4 fixed sm:mt-64 z-10'>
+    <nav className='sm:right-0 p-4 fixed sm:items-center top-1/2 transform -translate-y-1/2 z-10'>
       <ul className='flex flex-row sm:flex-col items-center justify-center sm:py-10 m-5  rounded-full bg-purple-900 text-white'>
         <li>
           <button onClick={traducir}>
@@ -43,17 +43,17 @@ export default function Navbar() {
             </Link>
         </li>
         <li className='hover:cursor-pointer m-5'>
-          <Link to="sobreMi" smooth={true} duration={500} offset={-100}>
+          <Link to="sobreMi" smooth={true} duration={500} offset={0}>
             <img src={person} alt="" width={50} className='transform transition-transform hover:scale-110 hover:cursor-pointe'/>
           </Link>
         </li>
         <li className='hover:cursor-pointer m-5'>
-          <Link to="tecnologias" smooth={true} duration={500} offset={-80}>
+          <Link to="tecnologias" smooth={true} duration={500} offset={0}>
             <img src={lightbulb} alt="" width={50} className='transform transition-transform hover:scale-110 hover:cursor-pointe' />
           </Link>
         </li>
         <li className='hover:cursor-pointer m-5'>
-          <Link to="experiencia" smooth={true} duration={500} offset={-80}>
+          <Link to="experiencia" smooth={true} duration={500} offset={0}>
             <img src={experience} alt="" width={50} className='transform transition-transform hover:scale-110 hover:cursor-pointe'/>
           </Link>
         </li>
