5364af9d9cdad8264a2b2be9f2cd0a5fae7b3848
GeniuX <geniuh7@gmail.com>
2023-12-30 11:28:39 -0800

    more responsive proyects

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -11,11 +11,11 @@ export default function Experiencia() {
       {proyectos.map((value,index)=>
 
     <a key={index} href={value.url} target="_blank"className='m-64'>
-    <div  className='my-32 p-1 rounded-xl flex flex-col md:flex-row g-black transform transition-transform hover:scale-110 hover:cursor-pointer'>
-        <div className='sm:w-2/3  '>
+    <div  className='my-32 p-1 rounded-xl flex flex-cold lg:flex-row g-black transform transition-transform hover:scale-110 hover:cursor-pointer'>
+        <div className='lg:w-2/3  '>
           <img src={value.imagen} alt="" />
         </div>
-        <div className='sm:w-1/3 max-h-max p-3 flex flex-col justify-between   '>
+        <div className='lg:w-1/3 max-h-max p-3 flex flex-col justify-between   '>
             <p className='text-xl text-yellow-500'>{value.titulo}</p>
             <p className='mt-2'>{value.descripcion}</p>
             <div className='flex flex-row justify-start flex-wrap '>
